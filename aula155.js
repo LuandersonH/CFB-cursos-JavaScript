3e5b3d50b2db7dc942754ead5f49c6c81ccec3d4
LuandersonH <work.luanderson@gmail.com>
2024-07-28 15:27:20 -0300

    155 e 156 finalizadas

--- aula155.js
+++ aula155.js
@@ -1,5 +1,5 @@
 const configdgv = {
-    endpoint: "aula154.json",
+    endpoint: "aula155.json",
     idDestino: "dgvDados"
 } 
 
@@ -14,29 +14,43 @@ const dgv=(configdgv) => {
             dvgLinha.setAttribute("class", "dgvLinha");
 
             const c1 = document.createElement("div");
-            c1.setAttribute("class", "c1");
+            c1.setAttribute("class", "coluna c1");
             c1.innerHTML = el.id;
             dvgLinha.appendChild(c1)
 
             const c2 = document.createElement("div");
-            c2.setAttribute("class", "c2");
+            c2.setAttribute("class", "coluna c2");
             c2.innerHTML = el.produto;
             dvgLinha.appendChild(c2)
 
             const c3 = document.createElement("div");
-            c3.setAttribute("class", "c3");
+            c3.setAttribute("class", "coluna c3");
             c3.innerHTML = el.marca;
             dvgLinha.appendChild(c3)
 
             const c4 = document.createElement("div");
-            c4.setAttribute("class", "c4");
+            c4.setAttribute("class", "coluna c4");
             c4.innerHTML = el.modelo;
             dvgLinha.appendChild(c4)
 
             const c5 = document.createElement("div");
-            c5.setAttribute("class", "c5");
-            c5.innerHTML = "D E V";
+            c5.setAttribute("class", "coluna c5");
             dvgLinha.appendChild(c5)
+
+            const imgEditar = document.createElement("img");
+            imgEditar.setAttribute("class", "DgvIcone");
+            imgEditar.setAttribute("src", "img/editar.svg");
+            c5.appendChild(imgEditar)
+
+            const imgVizualizar = document.createElement("img");
+            imgVizualizar.setAttribute("class", "DgvIcone");
+            imgVizualizar.setAttribute("src", "img/vizualizar.svg");
+            c5.appendChild(imgVizualizar)
+
+            const imgDeletar = document.createElement("img");
+            imgDeletar.setAttribute("class", "DgvIcone");
+            imgDeletar.setAttribute("src", "img/deletar.svg");
+            c5.appendChild(imgDeletar)
 
             dgvDados.appendChild(dvgLinha)
         });
